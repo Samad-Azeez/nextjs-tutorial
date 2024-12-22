293d4885ce487f74b69ddbe2b916bb69ff9ccfca
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-23 00:49:20 +0100

    feat: add local image display in tour page component

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,8 +1,26 @@
+import hijabImg from '@/images/hijab.jpg';
+import Image from 'next/image';
 async function page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return (
     <div>
       <h1 className='text-4xl'>ID : {id}</h1>
+      <section className='flex gap-x-4 mt-4'>
+        {/* local image */}
+        <div>
+          <Image
+            src={hijabImg}
+            alt='hijab'
+            width={300}
+            height={300}
+            className='w-[300] h-[300] object-cover rounded'
+            priority
+          />
+          <h2>local image</h2>
+        </div>
+        {/* remote image */}
+        <div></div>
+      </section>
     </div>
   );
 }

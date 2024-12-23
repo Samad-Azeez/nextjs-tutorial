a0388298e226befd6d75f7610d7c5eaed71e06d5
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-23 01:28:22 +0100

    feat: implement API data fetching and rendering for tours in ToursPage component

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+// API endpoint for fetching tours data
 const url = 'https://www.course-api.com/react-tours-project';
 
+// Define the Tour type
 type Tour = {
   id: string;
   name: string;
@@ -11,16 +13,21 @@ type Tour = {
   price: string;
 };
 
+// Async function to fetch and display tours
 async function ToursPage() {
+  // Fetch data from the API
   const response = await fetch(url);
   const data: Tour[] = await response.json();
   console.log(data);
+
+  // Render the tours
   return (
     <section>
       <h1 className='text-3xl mb-4'>Tours</h1>
       <div className='grid md:grid-cols-2 gap-8'>
         {data.map((tour) => {
           return (
+            // Link to the tour details page
             <Link
               className='hover:text-blue-500'
               key={tour.id}

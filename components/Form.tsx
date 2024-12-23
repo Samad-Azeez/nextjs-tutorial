34106c3503068a77037044c96cb01ec95bf22bf5
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-23 13:24:30 +0100

    feat: implement user creation form with input fields and submit button

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,5 +1,43 @@
+// Asynchronous function to create a user
+const createUser = async () => {
+  'use server';
+  console.log('creating user ...');
+};
+
+// Form component
 function Form() {
-  return <div>Form</div>;
+  return (
+    <form action={createUser} className={formStyle}>
+      <h2 className='text-2xl capitalize mb-4'>create user</h2>
+      {/* Input field for first name */}
+      <input
+        type='text'
+        name='firstName'
+        defaultValue='samad'
+        required
+        className={inputStyle}
+      />
+      {/* Input field for last name */}
+      <input
+        type='text'
+        name='lastName'
+        defaultValue='azeez'
+        required
+        className={inputStyle}
+      />
+      {/* Submit button */}
+      <button type='submit' className={btnStyle}>
+        submit
+      </button>
+    </form>
+  );
 }
 
+// CSS styles for the form
+const formStyle = 'max-w-lg flex flex-col gap-y-4 shadow rounded p-8';
+const inputStyle = 'border shadow rounded py-2 px-3 text-gray-700';
+const btnStyle =
+  'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded capitalize';
+
+// Export the Form component as the default export
 export default Form;

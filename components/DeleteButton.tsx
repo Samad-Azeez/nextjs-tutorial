ce2c49d1c5467f1a8e4f8d0db45f774b61cc6443
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-27 20:09:30 +0100

    feat: update DeleteButton to use removeUser action for user deletion

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,6 +1,11 @@
+import { deleteUser, removeUser } from '@/app/utils/actions';
+
 function DeleteButton({ id }: { id: string }) {
+  const removeUserWithId = removeUser.bind(null, id);
+
   return (
-    <form action=''>
+    <form action={removeUserWithId}>
+      <input type='hidden' name='name' value='test' />
       <button
         type='submit'
         className='bg-red-500 text-white text-xs rounded p-2'

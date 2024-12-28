b36a163c0579870a77ef628d6263ded1ccd668bb
Samad-Azeez <azeezsamad2004@gmail.com>
2024-12-28 10:34:31 +0100

    feat: enhance user API with GET and POST request handling

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,23 @@
 import { fetchUsers, saveUser } from '@/app/utils/actions';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const GET = async () => {
+/** GET REQUEST */
+export const GET = async (req: NextRequest) => {
+  console.log(req.url);
+  console.log(req.nextUrl.searchParams.get('id'));
+
   const users = await fetchUsers();
-  return Response.json({ users });
+  //   override the current path to that of the home page
+  // return NextResponse.redirect(new URL('/', req.url));
+  return NextResponse.json({ users });
+};
+
+/** POST REQUEST */
+export const POST = async (req: NextRequest) => {
+  const user = await req.json();
+  const newUser = { ...user, id: Date.now().toString() };
+
+  await saveUser(newUser);
+
+  return Response.json({ msg: 'user created' });
 };
